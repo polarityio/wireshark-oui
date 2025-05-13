6416754c0269bcf5f94b7dce9c1030ea54590797
Ed <ed@polarity.io>
2025-05-12 21:01:43 -0400

    Update README with new option

--- config/config.js
+++ config/config.js
@@ -28,7 +28,17 @@ module.exports = {
       key: 'autoUpdate',
       name: 'Automatically Update OUI Database',
       description:
-        "If enabled, the integration will automatically update the OUI database from Wireshark's download server once a week.",
+        "If enabled, the integration will automatically update the OUI database from Wireshark's download server once a week. Defaults to enabled.",
+      default: true,
+      type: 'boolean',
+      userCanEdit: false,
+      adminOnly: true
+    },
+    {
+      key: 'returnMisses',
+      name: 'Always return results',
+      description:
+        'If enabled, the integration will return a result even if the MAC address has no vendor assignment.  Defaults to enabled.',
       default: true,
       type: 'boolean',
       userCanEdit: false,
